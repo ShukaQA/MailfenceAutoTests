c32095678a2dc50a3e60a8703458b753b8970234
Giorgi.Shukakidze <gshukakidzeqa@gmail.com>
2025-04-07 15:31:29 +0400

    fixed PropertyLoader

--- src/main/java/core/utils/PropertyLoader.java
+++ src/main/java/core/utils/PropertyLoader.java
@@ -2,38 +2,45 @@ package core.utils;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 public class PropertyLoader {
+
+    private static final Map<String, Properties> propertiesCache = new HashMap<>();
 
     private PropertyLoader() {
     }
 
     public static String returnConfigValue(final String property, String fileName) {
-        Properties properties = new Properties();
-        InputStream propFileInpStream = null;
-        try {
-            propFileInpStream = PropertyLoader.class.getClassLoader().getResourceAsStream(fileName);
-            if (propFileInpStream == null) {
-                throw new IOException("Property file '" + fileName + "' not found in the classpath.");
-            }
-            properties.load(propFileInpStream);
-            String value = properties.getProperty(property);
-            if (value == null) {
-                throw new IllegalArgumentException("Property '" + property + "' not found in the file '" + fileName + "'.");
-            }
-            return value;
-        } catch (IOException | IllegalArgumentException e) {
-            System.err.println("Error loading property: " + e.getMessage());
-            return null;
-        } finally {
-            if (propFileInpStream != null) {
-                try {
-                    propFileInpStream.close();
-                } catch (IOException e) {
-                    System.err.println("Failed to close the InputStream: " + e.getMessage());
-                }
+        Properties properties = propertiesCache.get(fileName);
+
+        System.out.println(properties);
+        if (properties == null) {
+            properties = loadProperties(fileName);
+            if (properties != null) {
+                propertiesCache.put(fileName, properties);
+                System.out.println(properties);
             }
         }
+        assert properties != null;
+        return properties.getProperty(property);
+    }
+
+
+    private static Properties loadProperties(String fileName) {
+        Properties properties = new Properties();
+        try (InputStream propFileInpStream = PropertyLoader.class.getClassLoader().getResourceAsStream(fileName)) {
+            if (propFileInpStream == null) {
+                System.err.println("Property file '" + fileName + "' not found in the classpath.");
+                return null;
+            }
+            properties.load(propFileInpStream);
+        } catch (IOException e) {
+            System.err.println("Error loading property file '" + fileName + "': " + e.getMessage());
+            return null;
+        }
+        return properties;
     }
 }
